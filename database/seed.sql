b6799490fb2168849eb1884e3b577e45d1658b7e
matthijsdam <matthijs.dam@amsterdam.nl>
2023-05-05 14:38:34 +0200

    Added regions seed

---
+++ database/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO `regions`(name)
+VALUES ('Utrecht'),
+       ('Noord-Holland'),
+       ('Zuid-Holland'),
+       ('Flevoland'),
+       ('Overijssel'),
+       ('Drenthe'),
+       ('Gelderland'),
+       ('Limburg'),
+       ('Noord-Brabant'),
+       ('Zeeland');
