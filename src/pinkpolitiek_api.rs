3a42111c137d25cfc87faec967ab9b9b4f377a5e
matthijsdam <matthijs.dam@amsterdam.nl>
2023-07-05 17:43:47 +0200

    restructure get events code to be more complete

--- src/pinkpolitiek_api.rs
+++ src/pinkpolitiek_api.rs
@@ -1,4 +1,6 @@
 use std::env;
+use actix_web::http::StatusCode;
+use actix_web::HttpResponse;
 use actix_web::web::Data;
 use serde::{Serialize, Deserialize};
 use crate::AppState;
@@ -49,38 +51,67 @@ pub struct PPEventsData {
     total_pages: i32,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct PPLoginResponse {
+    pub token: String,
+    pub user_email: String,
+    pub user_nicename: String,
+    pub user_display_name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PPErrorData {
+    status: i32,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PPErrorResponse {
+    code: String,
+    message: String,
+    data: PPErrorData,
+}
+
 pub async fn get_events(app_state: Data<AppState>) -> Result<Vec<PPEvent>, String> {
     let response = reqwest::get(
         env::var("PINKPOLITIEK_URL").unwrap() + "/tribe/events/v1/events"
-    ).await;
+    );
 
-    if response.is_err() {
+    let Ok(response) = response.await else {
         return Err("Could not connect to ".to_owned() + &env::var("PINKPOLITIEK_URL").unwrap());
-    }
+    };
 
-    let events = response
-        .unwrap()
-        .json::<PPEventsData>()
-        .await
-        .unwrap()
-        .events;
+    return match response.status() {
+        StatusCode::OK => {
+            let events = response
+                .json::<PPEventsData>()
+                .await
+                .unwrap()
+                .events;
 
-    match store_to_db(app_state, &events).await {
-        Ok(_) => Ok(events),
-        Err(message) => Err(message)
-    }
+            match store_to_db(app_state, &events).await {
+                Ok(_) => Ok(events),
+                Err(message) => Err(message)
+            }
+        },
+        _ => {
+            Err(response.json::<PPErrorResponse>().await.unwrap().message)
+        }
+    };
+
 }
 
 async fn store_to_db(app_state: Data<AppState>, events: &Vec<PPEvent>) -> Result<(), String> {
     for event in events {
         let result = sqlx::query!(r#"
-            INSERT INTO events (id, title, description, source, url)
-            VALUES(?, ?, ?, 'external', ?) AS n ON DUPLICATE KEY UPDATE
-            title = n.title, description = n.description, source = n.source, url = n.url"#,
+            INSERT INTO events (id, title, description, source, url, start_date, all_day)
+            VALUES(?, ?, ?, 'external', ?, ?, ?) AS n ON DUPLICATE KEY UPDATE
+            title = n.title, description = n.description, source = n.source, url = n.url, start_date = n.start_date, all_day = n.all_day"#,
             event.id,
             event.title,
             event.description,
             event.url,
+            event.start_date,
+            event.all_day,
         ).execute(&app_state.pool).await;
 
         if result.is_err() {
